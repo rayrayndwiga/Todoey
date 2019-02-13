486b1d9f65a24db0af784e6517790d4e34e9fdc0
Brian Ndwiga <ndwigabrian@gmail.com>
2019-02-13 11:33:06 +0800

    Convert to MVC design pattern

---
+++ Todoey/Models/Item.swift
@@ -0,0 +1,19 @@
+//
+//  Todo.swift
+//  Todoey
+//
+//  Created by Brian Ndwiga on 13/2/19.
+//  Copyright © 2019 Brian Ndwiga. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title = ""
+    var done  = false
+    
+//    init(_title:String,_done:Bool = false) {
+//        title = _title
+//        done = _done
+//    }
+}
